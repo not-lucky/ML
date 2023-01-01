02b3684d113194b2dfe07da2bc53e9343a78ee59
not-lucky <57046712+not-lucky@users.noreply.github.com>
2023-01-01 12:00:51 +0530

    We'll figure it out on Monday

--- Shakespear/shakespear.py
+++ Shakespear/shakespear.py
@@ -1,24 +1,6 @@
 import tensorflow as tf
 
-tf.debugging.set_log_device_placement(True)
-
-# Create some tensors
-a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
-b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
-c = tf.matmul(a, b)
-
-print(c)
-
-tf.debugging.set_log_device_placement(True)
-
-# Place tensors on the CPU
-with tf.device('/CPU:0'):
-    a = tf.constant([[1.0, 2.0, 3.0], [4.0, 5.0, 6.0]])
-    b = tf.constant([[1.0, 2.0], [3.0, 4.0], [5.0, 6.0]])
-
-# Run on the GPU
-c = tf.matmul(a, b)
-print(c)
+tf.config.list_physical_devices()  # APU device is visible to TensorFlow.
 
 # import tensorflow as tf
 
@@ -26,252 +8,260 @@ print(c)
 # import os
 # import time
 
-# path_to_file = tf.keras.utils.get_file(
-#     'shakespeare.txt',
-#     'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt'
-# )
+# # path_to_file = tf.keras.utils.get_file(
+# #     'shakespeare.txt',
+# #     'https://storage.googleapis.com/download.tensorflow.org/data/shakespeare.txt'
+# # )
 
-# # Read, then decode for py2 compat.
-# text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
-# # length of text is the number of characters in it
-# print(f'Length of text: {len(text)} characters')
+# # # Read, then decode for py2 compat.
+# # text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
+# # # length of text is the number of characters in it
+# # print(f'Length of text: {len(text)} characters')
 
-# # Take a look at the first 250 characters in text
-# print(text[:250])
+# # # Take a look at the first 250 characters in text
+# # print(text[:250])
 
-# # The unique characters in the file
-# vocab = sorted(set(text))
-# print(f'{len(vocab)} unique characters')
+# # # The unique characters in the file
+# # vocab = sorted(set(text))
+# # print(f'{len(vocab)} unique characters')
 
-# example_texts = ['abcdefg', 'xyz']
+# # example_texts = ['abcdefg', 'xyz']
 
-# chars = tf.strings.unicode_split(example_texts, input_encoding='UTF-8')
-# chars
+# # chars = tf.strings.unicode_split(example_texts, input_encoding='UTF-8')
+# # chars
 
-# ids_from_chars = tf.keras.layers.StringLookup(vocabulary=list(vocab),
-#                                               mask_token=None)
+# # ids_from_chars = tf.keras.layers.StringLookup(vocabulary=list(vocab),
+# #                                               mask_token=None)
 
-# ids = ids_from_chars(chars)
-# ids
+# # ids = ids_from_chars(chars)
+# # ids
 
-# chars_from_ids = tf.keras.layers.StringLookup(
-#     vocabulary=ids_from_chars.get_vocabulary(), invert=True, mask_token=None)
+# # chars_from_ids = tf.keras.layers.StringLookup(
+# #     vocabulary=ids_from_chars.get_vocabulary(), invert=True, mask_token=None)
 
-# chars = chars_from_ids(ids)
-# chars
+# # chars = chars_from_ids(ids)
+# # chars
 
-# tf.strings.reduce_join(chars, axis=-1).numpy()
+# # tf.strings.reduce_join(chars, axis=-1).numpy()
 
-# def text_from_ids(ids):
-#     return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
 
-# all_ids = ids_from_chars(tf.strings.unicode_split(text, 'UTF-8'))
-# all_ids
+# # def text_from_ids(ids):
+# #     return tf.strings.reduce_join(chars_from_ids(ids), axis=-1)
 
-# ids_dataset = tf.data.Dataset.from_tensor_slices(all_ids)
 
-# for ids in ids_dataset.take(10):
-#     print(chars_from_ids(ids).numpy().decode('utf-8'))
+# # all_ids = ids_from_chars(tf.strings.unicode_split(text, 'UTF-8'))
+# # all_ids
 
-# seq_length = 100
+# # ids_dataset = tf.data.Dataset.from_tensor_slices(all_ids)
 
-# sequences = ids_dataset.batch(seq_length + 1, drop_remainder=True)
+# # for ids in ids_dataset.take(10):
+# #     print(chars_from_ids(ids).numpy().decode('utf-8'))
 
-# for seq in sequences.take(1):
-#     print(chars_from_ids(seq))
+# # seq_length = 100
 
-# for seq in sequences.take(5):
-#     print(text_from_ids(seq).numpy())
+# # sequences = ids_dataset.batch(seq_length + 1, drop_remainder=True)
 
-# def split_input_target(sequence):
-#     input_text = sequence[:-1]
-#     target_text = sequence[1:]
-#     return input_text, target_text
+# # for seq in sequences.take(1):
+# #     print(chars_from_ids(seq))
 
-# split_input_target(list("Tensorflow"))
+# # for seq in sequences.take(5):
+# #     print(text_from_ids(seq).numpy())
 
-# dataset = sequences.map(split_input_target)
 
-# for input_example, target_example in dataset.take(1):
-#     print("Input :", text_from_ids(input_example).numpy())
-#     print("Target:", text_from_ids(target_example).numpy())
+# # def split_input_target(sequence):
+# #     input_text = sequence[:-1]
+# #     target_text = sequence[1:]
+# #     return input_text, target_text
 
-# # Batch size
-# BATCH_SIZE = 64
 
-# # Buffer size to shuffle the dataset
-# # (TF data is designed to work with possibly infinite sequences,
-# # so it doesn't attempt to shuffle the entire sequence in memory. Instead,
-# # it maintains a buffer in which it shuffles elements).
-# BUFFER_SIZE = 10000
+# # split_input_target(list("Tensorflow"))
 
-# dataset = (dataset.shuffle(BUFFER_SIZE).batch(
-#     BATCH_SIZE, drop_remainder=True).prefetch(tf.data.experimental.AUTOTUNE))
+# # dataset = sequences.map(split_input_target)
 
-# dataset
+# # for input_example, target_example in dataset.take(1):
+# #     print("Input :", text_from_ids(input_example).numpy())
+# #     print("Target:", text_from_ids(target_example).numpy())
 
-# # Length of the vocabulary in StringLookup Layer
-# vocab_size = len(ids_from_chars.get_vocabulary())
+# # # Batch size
+# # BATCH_SIZE = 64
 
-# # The embedding dimension
-# embedding_dim = 256
+# # # Buffer size to shuffle the dataset
+# # # (TF data is designed to work with possibly infinite sequences,
+# # # so it doesn't attempt to shuffle the entire sequence in memory. Instead,
+# # # it maintains a buffer in which it shuffles elements).
+# # BUFFER_SIZE = 10000
 
-# # Number of RNN units
-# rnn_units = 1024
+# # dataset = (dataset.shuffle(BUFFER_SIZE).batch(
+# #     BATCH_SIZE, drop_remainder=True).prefetch(tf.data.experimental.AUTOTUNE))
 
-# class MyModel(tf.keras.Model):
+# # dataset
 
-#     def __init__(self, vocab_size, embedding_dim, rnn_units):
-#         super().__init__(self)
-#         self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
-#         self.gru = tf.keras.layers.GRU(rnn_units,
-#                                        return_sequences=True,
-#                                        return_state=True)
-#         self.dense = tf.keras.layers.Dense(vocab_size)
+# # # Length of the vocabulary in StringLookup Layer
+# # vocab_size = len(ids_from_chars.get_vocabulary())
 
-#     def call(self, inputs, states=None, return_state=False, training=False):
-#         x = inputs
-#         x = self.embedding(x, training=training)
-#         if states is None:
-#             states = self.gru.get_initial_state(x)
-#         x, states = self.gru(x, initial_state=states, training=training)
-#         x = self.dense(x, training=training)
+# # # The embedding dimension
+# # embedding_dim = 256
 
-#         if return_state:
-#             return x, states
-#         else:
-#             return x
+# # # Number of RNN units
+# # rnn_units = 1024
 
-# model = MyModel(vocab_size=vocab_size,
-#                 embedding_dim=embedding_dim,
-#                 rnn_units=rnn_units)
 
-# for input_example_batch, target_example_batch in dataset.take(1):
-#     example_batch_predictions = model(input_example_batch)
-#     print(example_batch_predictions.shape,
-#           "# (batch_size, sequence_length, vocab_size)")
+# # class MyModel(tf.keras.Model):
 
-# model.summary()
+# #     def __init__(self, vocab_size, embedding_dim, rnn_units):
+# #         super().__init__(self)
+# #         self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+# #         self.gru = tf.keras.layers.GRU(rnn_units,
+# #                                        return_sequences=True,
+# #                                        return_state=True)
+# #         self.dense = tf.keras.layers.Dense(vocab_size)
 
-# sampled_indices = tf.random.categorical(example_batch_predictions[0],
-#                                         num_samples=1)
-# sampled_indices = tf.squeeze(sampled_indices, axis=-1).numpy()
+# #     def call(self, inputs, states=None, return_state=False, training=False):
+# #         x = inputs
+# #         x = self.embedding(x, training=training)
+# #         if states is None:
+# #             states = self.gru.get_initial_state(x)
+# #         x, states = self.gru(x, initial_state=states, training=training)
+# #         x = self.dense(x, training=training)
 
-# sampled_indices
+# #         if return_state:
+# #             return x, states
+# #         else:
+# #             return x
 
-# print("Input:\n", text_from_ids(input_example_batch[0]).numpy())
-# print()
-# print("Next Char Predictions:\n", text_from_ids(sampled_indices).numpy())
 
-# loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
+# # model = MyModel(vocab_size=vocab_size,
+# #                 embedding_dim=embedding_dim,
+# #                 rnn_units=rnn_units)
 
-# example_batch_mean_loss = loss(target_example_batch, example_batch_predictions)
-# print("Prediction shape: ", example_batch_predictions.shape,
-#       " # (batch_size, sequence_length, vocab_size)")
-# print("Mean loss:        ", example_batch_mean_loss)
+# # for input_example_batch, target_example_batch in dataset.take(1):
+# #     example_batch_predictions = model(input_example_batch)
+# #     print(example_batch_predictions.shape,
+# #           "# (batch_size, sequence_length, vocab_size)")
 
-# tf.exp(example_batch_mean_loss).numpy()
+# # model.summary()
 
-# model.compile(optimizer='adam', loss=loss)
+# # sampled_indices = tf.random.categorical(example_batch_predictions[0],
+# #                                         num_samples=1)
+# # sampled_indices = tf.squeeze(sampled_indices, axis=-1).numpy()
 
-# # Directory where the checkpoints will be saved
-# checkpoint_dir = './training_checkpoints'
-# # Name of the checkpoint files
-# checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
+# # sampled_indices
 
-# checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
-#     filepath=checkpoint_prefix, save_weights_only=True)
+# # print("Input:\n", text_from_ids(input_example_batch[0]).numpy())
+# # print()
+# # print("Next Char Predictions:\n", text_from_ids(sampled_indices).numpy())
 
-# EPOCHS = 20
+# # loss = tf.losses.SparseCategoricalCrossentropy(from_logits=True)
 
-# history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
+# # example_batch_mean_loss = loss(target_example_batch, example_batch_predictions)
+# # print("Prediction shape: ", example_batch_predictions.shape,
+# #       " # (batch_size, sequence_length, vocab_size)")
+# # print("Mean loss:        ", example_batch_mean_loss)
 
-# class OneStep(tf.keras.Model):
+# # tf.exp(example_batch_mean_loss).numpy()
 
-#     def __init__(self, model, chars_from_ids, ids_from_chars, temperature=1.0):
-#         super().__init__()
-#         self.temperature = temperature
-#         self.model = model
-#         self.chars_from_ids = chars_from_ids
-#         self.ids_from_chars = ids_from_chars
+# # model.compile(optimizer='adam', loss=loss)
 
-#         # Create a mask to prevent "[UNK]" from being generated.
-#         skip_ids = self.ids_from_chars(['[UNK]'])[:, None]
-#         sparse_mask = tf.SparseTensor(
-#             # Put a -inf at each bad index.
-#             values=[-float('inf')] * len(skip_ids),
-#             indices=skip_ids,
-#             # Match the shape to the vocabulary
-#             dense_shape=[len(ids_from_chars.get_vocabulary())])
-#         self.prediction_mask = tf.sparse.to_dense(sparse_mask)
+# # # Directory where the checkpoints will be saved
+# # checkpoint_dir = './training_checkpoints'
+# # # Name of the checkpoint files
+# # checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt_{epoch}")
 
-#     @tf.function
-#     def generate_one_step(self, inputs, states=None):
-#         # Convert strings to token IDs.
-#         input_chars = tf.strings.unicode_split(inputs, 'UTF-8')
-#         input_ids = self.ids_from_chars(input_chars).to_tensor()
+# # checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
+# #     filepath=checkpoint_prefix, save_weights_only=True)
 
-#         # Run the model.
-#         # predicted_logits.shape is [batch, char, next_char_logits]
-#         predicted_logits, states = self.model(inputs=input_ids,
-#                                               states=states,
-#                                               return_state=True)
-#         # Only use the last prediction.
-#         predicted_logits = predicted_logits[:, -1, :]
-#         predicted_logits = predicted_logits / self.temperature
-#         # Apply the prediction mask: prevent "[UNK]" from being generated.
-#         predicted_logits = predicted_logits + self.prediction_mask
+# # EPOCHS = 20
 
-#         # Sample the output logits to generate token IDs.
-#         predicted_ids = tf.random.categorical(predicted_logits, num_samples=1)
-#         predicted_ids = tf.squeeze(predicted_ids, axis=-1)
+# # history = model.fit(dataset, epochs=EPOCHS, callbacks=[checkpoint_callback])
 
-#         # Convert from token ids to characters
-#         predicted_chars = self.chars_from_ids(predicted_ids)
 
-#         # Return the characters and model state.
-#         return predicted_chars, states
+# # class OneStep(tf.keras.Model):
 
-# one_step_model = OneStep(model, chars_from_ids, ids_from_chars)
+# #     def __init__(self, model, chars_from_ids, ids_from_chars, temperature=1.0):
+# #         super().__init__()
+# #         self.temperature = temperature
+# #         self.model = model
+# #         self.chars_from_ids = chars_from_ids
+# #         self.ids_from_chars = ids_from_chars
 
-# start = time.time()
-# states = None
-# next_char = tf.constant(['ROMEO:'])
-# result = [next_char]
+# #         # Create a mask to prevent "[UNK]" from being generated.
+# #         skip_ids = self.ids_from_chars(['[UNK]'])[:, None]
+# #         sparse_mask = tf.SparseTensor(
+# #             # Put a -inf at each bad index.
+# #             values=[-float('inf')] * len(skip_ids),
+# #             indices=skip_ids,
+# #             # Match the shape to the vocabulary
+# #             dense_shape=[len(ids_from_chars.get_vocabulary())])
+# #         self.prediction_mask = tf.sparse.to_dense(sparse_mask)
 
-# for n in range(1000):
-#     next_char, states = one_step_model.generate_one_step(next_char,
-#                                                          states=states)
-#     result.append(next_char)
+# #     @tf.function
+# #     def generate_one_step(self, inputs, states=None):
+# #         # Convert strings to token IDs.
+# #         input_chars = tf.strings.unicode_split(inputs, 'UTF-8')
+# #         input_ids = self.ids_from_chars(input_chars).to_tensor()
 
-# result = tf.strings.join(result)
-# end = time.time()
-# print(result[0].numpy().decode('utf-8'), '\n\n' + '_' * 80)
-# print('\nRun time:', end - start)
+# #         # Run the model.
+# #         # predicted_logits.shape is [batch, char, next_char_logits]
+# #         predicted_logits, states = self.model(inputs=input_ids,
+# #                                               states=states,
+# #                                               return_state=True)
+# #         # Only use the last prediction.
+# #         predicted_logits = predicted_logits[:, -1, :]
+# #         predicted_logits = predicted_logits / self.temperature
+# #         # Apply the prediction mask: prevent "[UNK]" from being generated.
+# #         predicted_logits = predicted_logits + self.prediction_mask
 
-# start = time.time()
-# states = None
-# next_char = tf.constant(['ROMEO:', 'ROMEO:', 'ROMEO:', 'ROMEO:', 'ROMEO:'])
-# result = [next_char]
+# #         # Sample the output logits to generate token IDs.
+# #         predicted_ids = tf.random.categorical(predicted_logits, num_samples=1)
+# #         predicted_ids = tf.squeeze(predicted_ids, axis=-1)
 
-# for n in range(1000):
-#     next_char, states = one_step_model.generate_one_step(next_char,
-#                                                          states=states)
-#     result.append(next_char)
+# #         # Convert from token ids to characters
+# #         predicted_chars = self.chars_from_ids(predicted_ids)
 
-# result = tf.strings.join(result)
-# end = time.time()
-# print(result, '\n\n' + '_' * 80)
-# print('\nRun time:', end - start)
+# #         # Return the characters and model state.
+# #         return predicted_chars, states
 
-# tf.saved_model.save(one_step_model, 'one_step')
+
+# # one_step_model = OneStep(model, chars_from_ids, ids_from_chars)
+
+# # start = time.time()
+# # states = None
+# # next_char = tf.constant(['ROMEO:'])
+# # result = [next_char]
+
+# # for n in range(1000):
+# #     next_char, states = one_step_model.generate_one_step(next_char,
+# #                                                          states=states)
+# #     result.append(next_char)
+
+# # result = tf.strings.join(result)
+# # end = time.time()
+# # print(result[0].numpy().decode('utf-8'), '\n\n' + '_' * 80)
+# # print('\nRun time:', end - start)
+
+# # start = time.time()
+# # states = None
+# # next_char = tf.constant(['ROMEO:', 'ROMEO:', 'ROMEO:', 'ROMEO:', 'ROMEO:'])
+# # result = [next_char]
+
+# # for n in range(1000):
+# #     next_char, states = one_step_model.generate_one_step(next_char,
+# #                                                          states=states)
+# #     result.append(next_char)
+
+# # result = tf.strings.join(result)
+# # end = time.time()
+# # print(result, '\n\n' + '_' * 80)
+# # print('\nRun time:', end - start)
+
+# # tf.saved_model.save(one_step_model, 'one_step')
 # one_step_reloaded = tf.saved_model.load('one_step')
 
 # states = None
 # next_char = tf.constant(['ROMEO:'])
 # result = [next_char]
-
-# for n in range(100):
+# print("\n\n\n\n\n\n\n\n")
+# for n in range(1000):
 #     next_char, states = one_step_reloaded.generate_one_step(next_char,
 #                                                             states=states)
 #     result.append(next_char)
